9749fa6ab39c520f5e76390fe2a9b02d5ae7798f
Valentin Moreau <valentin.moreau34750@gmail.com>
2022-01-31 00:55:25 +0100

    Ajout d'un Footer

---
+++ react-app/src/components/Footer/Footer.js
@@ -0,0 +1,31 @@
+
+function Footer ({ isNotLogged }) {
+
+    let getToken = JSON.parse(localStorage.getItem('commandSignin'));
+
+    const deleteUser = () => {
+        fetch("http://localhost:3001/users/delete", {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': getToken[0].token
+            }
+        });
+        window.location.reload(false);
+    }
+
+    const logout = (evt) => {
+        evt.preventDefault();
+        localStorage.clear();
+        window.location.reload(false);
+    }
+
+    return (
+            <footer>
+                {isNotLogged ? "" : <button className="buttonDeleteAccount" onClick={deleteUser}>Supprimer mon compte</button>}
+                {isNotLogged ? "" : <button className="buttonLogout" onClick={logout}>Deconnexion</button>}
+            </footer>
+    );
+}
+
+export default Footer;
