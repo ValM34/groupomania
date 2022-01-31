9d1f42f0544ec6585cd496b65f5874da5b2ee210
Valentin Moreau <valentin.moreau34750@gmail.com>
2022-01-31 04:46:58 +0100

    Ajout de la variable procss.env.TOKEN_KEY

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization; // On sélectionne le token
-        const decodedToken = jwt.verify(token, "s7UcdjHZk33fiuMhH5in4Tu27BYhz6RCKFZCY82tU858bw_KA5QxDPvyi6xYdU7x7eF4z356223MPuwMe6YZSy5hz87x5N4R5BK9XS9z6up3MG_zxVB42TF748S3VmH2tMd6y67nQq5t6j8qhy9h6T3XG"); // On vérifie que le token est correct grâce à la clef
+        const decodedToken = jwt.verify(token, process.env.TOKEN_KEY); // On vérifie que le token est correct grâce à la clef
         const users_idusers = decodedToken.userId;
         req.auth = { users_idusers }; // On ajoute l'userId à l'objet req
         if (Number(req.body.users_idusers) && Number(req.body.users_idusers) !== users_idusers) { // On vérifie si l'userId a les autorisations nécessaires pour effectuer l'action qu'il souhaite
